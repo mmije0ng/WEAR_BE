741cff64f164dd7b31a5c4b76def6a42769c5b92
mmije0ng <victon777@naver.com>
2024-03-14 00:56:21 +0900

    [#1] Init: 개발 환경 세팅

---
+++ src/main/java/com/backend/wear/WearApplication.java
@@ -0,0 +1,13 @@
+package com.backend.wear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/wear/WearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.wear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
