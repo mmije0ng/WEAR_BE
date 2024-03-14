081d2b66740538c97396503218f8a4d5be7365ad
진미나 <mina200238@gmail.com>
2024-03-15 05:08:27 +0900

    Add: CI/CD ëfor test

---
+++ src/test/java/com/backend/wear/WearApplicationTests.java
@@ -0,0 +1,12 @@
+/*package com.backend.wear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class WearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
