374a7e0636790eaca014c7fa0700e010558aefed
진미나 <mina200238@gmail.com>
2024-03-15 23:23:47 +0900

    Build: add jar properties

---
+++ src/main/java/com/backend/wear/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.backend.wear.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+    @RequestMapping()
+    @GetMapping()
+    public String main(){
+        return "Hello World";
+    }
+}
