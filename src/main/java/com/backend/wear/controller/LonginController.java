0699fb34b6f61632af4dddbe0bd5ffb8a722ae66
mijeong <victon777@naver.com>
2024-03-23 09:32:40 +0900

    Fix: api key

--- src/main/java/com/backend/wear/controller/LonginController.java
+++ src/main/java/com/backend/wear/controller/LonginController.java
@@ -19,48 +19,48 @@ import java.util.Map;
 @RequestMapping("/api")
 public class LonginController {
 
-    @Value("${api.key}")
-    private String API_KEY;
-
-//    @PostMapping("/signup")
-//    public ResponseEntity<?> signUp(@RequestBody LoginDto)
-
-    //대학 인증 메일 발송
-    @PostMapping("/university/certify")
-    public ResponseEntity<?> certifyUniversity
-            (@RequestBody UnivCertRequestDto dto) throws IOException {
-
-        Map<String, Object> response =
-                UnivCert.certify(API_KEY, dto.getUniversityEmail(), dto.getUniversityName(), dto.isCheck());
-
-        return ResponseEntity.ok(response);
-    }
-
-    //메일로 발송된 인증 코드 입력
-    @PostMapping("/university/certifycode")
-    public ResponseEntity<?> certifyUserCode(
-        @RequestBody UnivCertRequestDto dto) throws IOException {
-
-        Map<String, Object> response =
-                UnivCert.certifyCode(API_KEY, dto.getUniversityEmail(),
-                        dto.getUniversityName(), dto.getCode());
-
-        Boolean success = (Boolean) response.get("success");
-        if (success) { //인증 성공
-            String universityName = (String) response.get("univName");
-            String universityEmail = (String) response.get("certified_email");
-
-            UnivCertResponseDto responseDto = new UnivCertResponseDto(universityEmail,universityName,success);
-
-            return ResponseEntity.ok(responseDto);
-        }
-
-        else { //인증 실패
-            Integer code = (Integer) response.get("code");
-            String message = (String) response.get("message");
-
-            UnivCertFail responseDto = new UnivCertFail(code,success,message);
-            return ResponseEntity.ok(responseDto);
-        }
-    }
+//    @Value("${api.key}")
+//    private String API_KEY;
+//
+////    @PostMapping("/signup")
+////    public ResponseEntity<?> signUp(@RequestBody LoginDto)
+//
+//    //대학 인증 메일 발송
+//    @PostMapping("/university/certify")
+//    public ResponseEntity<?> certifyUniversity
+//            (@RequestBody UnivCertRequestDto dto) throws IOException {
+//
+//        Map<String, Object> response =
+//                UnivCert.certify(API_KEY, dto.getUniversityEmail(), dto.getUniversityName(), dto.isCheck());
+//
+//        return ResponseEntity.ok(response);
+//    }
+//
+//    //메일로 발송된 인증 코드 입력
+//    @PostMapping("/university/certifycode")
+//    public ResponseEntity<?> certifyUserCode(
+//        @RequestBody UnivCertRequestDto dto) throws IOException {
+//
+//        Map<String, Object> response =
+//                UnivCert.certifyCode(API_KEY, dto.getUniversityEmail(),
+//                        dto.getUniversityName(), dto.getCode());
+//
+//        Boolean success = (Boolean) response.get("success");
+//        if (success) { //인증 성공
+//            String universityName = (String) response.get("univName");
+//            String universityEmail = (String) response.get("certified_email");
+//
+//            UnivCertResponseDto responseDto = new UnivCertResponseDto(universityEmail,universityName,success);
+//
+//            return ResponseEntity.ok(responseDto);
+//        }
+//
+//        else { //인증 실패
+//            Integer code = (Integer) response.get("code");
+//            String message = (String) response.get("message");
+//
+//            UnivCertFail responseDto = new UnivCertFail(code,success,message);
+//            return ResponseEntity.ok(responseDto);
+//        }
+//    }
 }
