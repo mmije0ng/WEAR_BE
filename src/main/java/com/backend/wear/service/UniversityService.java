65f603958398f2c19e62a936aa1af1952863df60
mijeong <victon777@naver.com>
2024-05-03 22:27:55 +0900

    Refactor: 인증 메일 발송 응답값 수정

--- src/main/java/com/backend/wear/service/UniversityService.java
+++ src/main/java/com/backend/wear/service/UniversityService.java
@@ -18,31 +18,36 @@ public class UniversityService {
         // 인증된 유저 리스트
         UnivCert.list(API_KEY);
 
+        //UnivCert.clear(API_KEY);
+
         // 인증 여부
         Map<String, Object> statusMap = UnivCert.status(API_KEY, certifyDto.getEmail());
         // 이미 인증된 사용자일 경우
         if(statusMap.get("success").equals(true)){
             statusMap.put("success", false);
+            statusMap.put("already_certified",true); //이미 인증된 이메일 여부
             statusMap.put("message","이미 인증된 이메일입니다.");
             return  statusMap;
         }
 
         Map<String, Object> certifyMap = UnivCert.certify(API_KEY, certifyDto.getEmail(), certifyDto.getUniversityName(), true);
-        if(certifyMap.get("success").equals(false))
-            certifyMap.put("message","대학교명을 잘 못 입력하였습니다. 올바른 대학교명을 입력해주세요.");
+        if(certifyMap.get("success").equals(false)){
+            certifyMap.put("correct",false);
+            certifyMap.put("message","대학교명을 잘 못 입력하였습니다.");
+        }
+
+       else
+           certifyMap.put("correct",true);
+
+        certifyMap.put("already_certified",false);
 
         return certifyMap;
-
-   //     UnivCert.clear(API_KEY);
-  //      UnivCert.clear(API_KEY, univCertDto.getEmail());
     }
 
     // 대학교 인증 코드 입력
     public Object certifyCode(UniversityRequestDto.CertifyCodeDto certifyCodeDto) throws IOException{
 
-        Map<String, Object> certifyCodeMap = UnivCert.certifyCode(API_KEY,
+        return UnivCert.certifyCode(API_KEY,
                 certifyCodeDto.getEmail(), certifyCodeDto.getUniversityName(),certifyCodeDto.getCode());
-
-        return certifyCodeMap;
     }
 }
